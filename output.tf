7e716e422464e7f178581923e5cc7bbc2fa76de3
Alejandro <alejandromartinezgonzalez@Alejandros-MacBook-Pro.local>
2024-01-27 16:01:50 -0700

    Try and improve output to finish script

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
 output "ip" {
-  value = [esxi_guest.vm[*].ip_address]
+  description = "VM's created Name, Private IP, RAM, CPU and Disk Space"
+  value = [esxi_guest.vm[*].guest_name, esxi_guest.vm[*].ip_address, esxi_guest.vm[*].memsize, esxi_guest.vm[*].numvcpus, esxi_guest.vm[*].boot_disk_size]
 }
